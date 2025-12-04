32d42efc08089f4b6f8fce0e2aa49a63c0acf4a5
Matheus de Paiva <matheuspaiva609@gmail.com>
2025-12-04 16:53:37 -0600

    atualização gerador.py

--- val/gerador.py
+++ val/gerador.py
@@ -1,10 +1,10 @@
 import random
 
-def gerarMatriz(linhas, colunas):
-    numeros = []
-    for i in range(10):
-        numeros.append([0] * 10)
-    return numeros
+def gerarMatriz(qtd_linhas:int, qtd_colunas:int)->list:
+    matriz = []
+    for i in range(qtd_linhas):
+        matriz.append([0] * qtd_colunas)
+    return matriz
 
 numeros = gerarMatriz(10, 10)
 
