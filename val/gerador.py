f39abde5554b374917cdefaa1950a17f982a4a5c
Matheus de Paiva <matheuspaiva609@gmail.com>
2025-12-04 18:19:08 -0600

    Novas Funções

--- val/gerador.py
+++ val/gerador.py
@@ -1,6 +1,6 @@
 import random
 
-def gerarMatriz(qtd_linhas:int, qtd_colunas:int, valor)->list:
+def gerarMatriz(qtd_linhas:int, qtd_colunas:int, valor=0)->list:
     matriz = []
     for i in range(qtd_linhas):
         matriz.append([valor] * qtd_colunas)
@@ -11,6 +11,34 @@ def preencher_matriz(matriz:list, menor=1, maior=100):
         for j in range(len(matriz)):
             numeros[i][j] = random.randint(menor, maior)
 
-numeros = gerarMatriz(10, 10, 4)
-preencherMatriz = preencher_matriz(numeros)
+def valores_distintos(matriz: list) -> list:
+    valor = []
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] not in valor:
+                valor.append(matriz[i][j])
+    return valor
+
+def verificar_ordenada(vetor: list)->bool:
+    for i in range(len(vetor)-1):
+        if (vetor[i] > vetor[i+1]):
+            return False
+        else:
+            return True
+
+numeros = gerarMatriz(10, 10)
+#print(numeros)
+
+preencher_matriz(numeros, 10, 20)
 print(numeros)
+
+print("//////////////////////////////////////")
+distintos = valores_distintos(numeros)
+print(distintos)
+print(len(distintos))
+print("*-" * 20)
+ordenada = verificar_ordenada(distintos)
+if ordenada == "True":
+    print('Ordenada')
+else:
+    print('Não Ordenada')
